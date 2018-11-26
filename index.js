1c61287b3cf49fad5dc217832f534da4ab7383a0
chunfengd <cdai@tetrascience.com>
2018-11-26 15:34:57 -0500

    sns-lambda

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const _ = require('lodash');
 
 module.exports.handler = async (event, context, callback) => {
   console.log(JSON.stringify(event, null, 2));
-  const message = _.get(event, ['Records', 0, 'body']);
+  // const message = _.get(event, ['Records', 0, 'body']); // sqs
+  const message = _.get(event, ['Records', 0, 'Sns', 'Message']); // sns
   console.log(message);
-  await bluebird.delay(55 * 1000);
+  await bluebird.delay(35 * 1000);
   console.log(message, 'done');
   return event;
 };
